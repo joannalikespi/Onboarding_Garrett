7058e352d5cd8233b2d8f9602fc37ccca3e3576a
garretje <garretje@rose-hulman.edu>
2020-09-29 02:37:09 -0400

    basic tweets displaying

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,10 +1,12 @@
 import React, { FC, Fragment, useEffect, useState } from 'react';
+import { Accordion, Button, Card } from 'react-bootstrap';
 
 // import ThemeSelector from './ThemeSelector';
 
 interface Tweet {
   text: string;
   videoPath?: string;
+  id: string;
 }
 
 const ThemeSelector: FC = () => {
@@ -18,19 +20,45 @@ const ThemeSelector: FC = () => {
   );
 };
 
-const TweetContainer: FC = () => {
-  useEffect((): void => {
-    console.log('Test');
-  }, []);
+type TweetContProps = {
+  tweets: Tweet[];
+};
+
+const TweetContainer = ({ tweets }: TweetContProps) => {
+  const [activeKeyState, setActiveKey] = useState<string>('');
+
+  const handleActiveKeyChange = (activeKey: string) => {
+    if (activeKeyState === activeKey) {
+      //allows us to close expanded item by clicking its toggle while open
+      activeKey = '';
+    }
+    setActiveKey(activeKey);
+  };
+
   return (
-    <Fragment>
-      <div>this is a tweet container</div>;
-    </Fragment>
+    <Accordion activeKey={activeKeyState}>
+      {tweets.map((tweet) => (
+        <Card key={tweet.id}>
+          <Card.Header>
+            <Accordion.Toggle
+              as={Button}
+              variant="link"
+              eventKey={tweet.id}
+              onClick={() => handleActiveKeyChange(tweet.id)}>
+              {tweet.text.substr(0, 15)}
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey={tweet.id}>
+            <Card.Body>{tweet.text.substr(15)}</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      ))}
+    </Accordion>
   );
 };
 
 const App: FC = () => {
-  const [tweets, setTweets] = useState<Tweet[]>();
+  const [tweetsList, setTweets] = useState<Tweet[]>();
 
   useEffect((): void => {
     (async () => {
@@ -40,18 +68,29 @@ const App: FC = () => {
           (response) => response.json() as Promise<Tweet[]>,
         );
       } while (!fetchedTweets.length);
+      let i = 0;
+      for (i; i < fetchedTweets.length; i++) {
+        fetchedTweets[i].id = i.toString(10);
+      }
       setTweets(fetchedTweets);
       // eslint-disable-next-line no-console
     })().catch(console.error);
   }, []);
-
   return (
     <Fragment>
       <ThemeSelector />
-      <TweetContainer />
-      {/* {tweets && <pre>{JSON.stringify(tweets, null, 4)}</pre>} */}
+      {tweetsExist(tweetsList)}
+      {/* {tweetsList && <pre>{JSON.stringify(tweetsList, null, 4)}</pre>} */}
     </Fragment>
   );
+};
+
+const tweetsExist = (tweetsList) => {
+  if (tweetsList) {
+    return <TweetContainer tweets={tweetsList} />;
+  } else {
+    return <Fragment />;
+  }
 };
 
 export default App;
