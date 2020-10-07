76150aa876f8865bee0faf92987428368bdc66f1
garretje <garretje@rose-hulman.edu>
2020-10-06 20:38:24 -0400

    removed outdated import

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './app';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'video-react/dist/video-react.css'; // import css
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
